b304ef7224b249906dbbbe193741413f7dd2e267
Murphy <62220780+StrangeGirlMurph@users.noreply.github.com>
2022-04-14 17:35:53 +0200

    logger
    Co-authored-by: Commentator2.0 <46420447+KommentatorForAll@users.noreply.github.com>

--- src/events/sampleEvent.ts
+++ src/events/sampleEvent.ts
@@ -2,5 +2,5 @@
 // and edit the parameter to fit in its type and name to the event
 export default async function interactionName(args: unknown) {
 	// do things if the event occurs
-	console.log(args);
+	logger.debug(args);
 }
