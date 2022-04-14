ef812848373902d4790756bd88fde7eaa28ab032
Murph <62220780+StrangeGirlMurph@users.noreply.github.com>
2022-04-14 15:11:51 +0200

    pretty

--- src/commands/sampleCommand.ts
+++ src/commands/sampleCommand.ts
@@ -4,27 +4,27 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 
 // set the class name, the export at the bottom and the file name to your desired command name (same as the one in the register function)
 class CommandName extends Command {
-    constructor() {
-        super("bunny"); // the name under which the bot internally stores your command (should be the same as the named set in `register`, must be unique)
-    }
+	constructor() {
+		super("bunny"); // the name under which the bot internally stores your command (should be the same as the named set in `register`, must be unique)
+	}
 
-    async execute(interaction: CommandInteraction): Promise<void> {
-        // put the logic of your command here
-        // for example:
-        const num = interaction.options.getInteger("amount") || 1;
-        await interaction.reply("🐇".repeat(num));
-    }
+	async execute(interaction: CommandInteraction): Promise<void> {
+		// put the logic of your command here
+		// for example:
+		const num = interaction.options.getInteger("amount") || 1;
+		await interaction.reply("🐇".repeat(num));
+	}
 
-    register(): SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommandGroup" | "addSubcommand"> {
-        // set the name and description shown in discord here (these are necessary)
-        // (name: string.length = 1-32, description string.length = 1-100)(the name has to be lowercase!)
-        // here you can also add things like options to your command
-        // see more under https://discord.js.org/#/docs/builders/stable/class/SlashCommandBuilder
-        return new SlashCommandBuilder()
-            .setName("bunny")
-            .setDescription("shows you a cute bunny")
-            .addIntegerOption(option => option.setName("amount").setDescription("number of bunnies").setRequired(false));
-    }
+	register(): SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommandGroup" | "addSubcommand"> {
+		// set the name and description shown in discord here (these are necessary)
+		// (name: string.length = 1-32, description string.length = 1-100)(the name has to be lowercase!)
+		// here you can also add things like options to your command
+		// see more under https://discord.js.org/#/docs/builders/stable/class/SlashCommandBuilder
+		return new SlashCommandBuilder()
+			.setName("bunny")
+			.setDescription("shows you a cute bunny")
+			.addIntegerOption((option) => option.setName("amount").setDescription("number of bunnies").setRequired(false));
+	}
 }
 
 export default new CommandName();
--- src/events/sampleEvent.ts
+++ src/events/sampleEvent.ts
@@ -1,5 +1,6 @@
-// set the function name to the event name 
+// set the function name to the event name
 // and edit the parameter to fit in its type and name to the event
 export default async function interactionName(args: unknown) {
-    // do things if the event occurs 
+	// do things if the event occurs
+	console.log(args);
 }
