6c46129ca454843c0840f9f02d7b484cbad0a1eb
Murph <62220780+StrangeGirlMurph@users.noreply.github.com>
2022-04-14 17:51:47 +0200

    logger

--- src/events/sampleEvent.ts
+++ src/events/sampleEvent.ts
@@ -1,3 +1,5 @@
+import { logger } from "logger"; // only there for logging
+
 // set the function name to the event name
 // and edit the parameter to fit in its type and name to the event
 export default async function interactionName(args: unknown) {
