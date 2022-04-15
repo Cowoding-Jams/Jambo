b1fae7178a1d109af73b363e6e5918411488f7c9
Murphy <62220780+StrangeGirlMurph@users.noreply.github.com>
2022-04-15 11:28:00 +0200

    Updating the sample file handling (#15)
    * revolutionizing the sample files
    
    * pretty
    
    * logger
    
    Co-authored-by: Commentator2.0 <46420447+KommentatorForAll@users.noreply.github.com>
    
    * function
    
    * pretty
    
    * logger
    
    Co-authored-by: Commentator2.0 <46420447+KommentatorForAll@users.noreply.github.com>

---
+++ src/events/sampleEvent.ts
@@ -0,0 +1,8 @@
+import { logger } from "logger"; // only there for logging
+
+// set the function name to the event name
+// and edit the parameter to fit in its type and name to the event
+export default async function interactionName(args: unknown) {
+	// do things if the event occurs
+	logger.debug(args);
+}
