23ec10ae91d60761d9dcc5cb535a0150ff56ab28
Murphy <62220780+StrangeGirlMurph@users.noreply.github.com>
2022-04-22 14:22:14 +0200

    doc comment
    Co-authored-by: BlackDemonFire <42120952+BlackDemonFire@users.noreply.github.com>

--- src/util/random.ts
+++ src/util/random.ts
@@ -6,7 +6,9 @@ export function pickRandomFromList<T>(list: T[]): T {
 	return list[randInt(0, list.length)];
 }
 
-// upperbound is not included
+/** 
+ * upperbound is not included
+ */
 export function randInt(lowerbound: number, upperbound: number) {
 	return lowerbound + Math.floor(Math.random() * (upperbound - lowerbound));
 }
