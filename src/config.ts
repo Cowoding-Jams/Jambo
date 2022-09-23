e36641645672f84cf30963a6bc0552e94682f60c
Murphy <62220780+StrangeGirlMurph@users.noreply.github.com>
2022-09-23 16:42:48 +0200

    welcome message for new members

--- src/config.ts
+++ src/config.ts
@@ -30,6 +30,7 @@ export const config: BotConfig = {
 		["Sea Serpent", "#5db9cf"],
 	],
 	color: "#F0A5AC",
+	serverDescription: "We're a group of young and mostly queer people having a game jam/hackathon server together. We're a very friendly and welcoming community and are happy to have you join us! \nCheck out <#1022874504525008997> for more information!",
 	logLevel: "debug",
 };
 
@@ -46,6 +47,8 @@ interface BotConfig {
 	colorRoles: [string, ColorResolvable][];
 	// The main color the bot will use.
 	color: ColorResolvable;
+	// A description of the server that will be displayed in the welcome message.
+	serverDescription: string;
 	// Also available: error, warn, info, http, verbose, debug, silly.
 	logLevel: "debug" | "info" | "warn" | "error" | "verbose";
 }
---
+++ src/events/guildMemberAdd.ts
@@ -0,0 +1,30 @@
+import { GuildMember, EmbedBuilder } from "discord.js";
+import { logger } from "../logger";
+import { addDefaultEmbedFooter } from "../util/embeds";
+import { config } from "../config";
+import {inlineCode} from "@discordjs/builders";
+
+export default async function guildMemberAdd(member: GuildMember) {
+	let embed = new EmbedBuilder()
+		.setTitle(`Hey ${member.displayName} 😊`)
+		.setThumbnail(member.guild.iconURL({ size: 1024 }) || "")
+		.setDescription(`You are the ${member.guild.memberCount}th member of this server!`)
+		.addFields(
+			{
+				name: `Welcome on ${member.guild.name}!`,
+				value: config.serverDescription,
+			},
+			{
+				name: `I am ${config.botName} <3`,
+				value: `I manage this whole server from creating polls to welcoming new members like you. I have a lot of cool features. You can find out more about them by using ${inlineCode("/help")} in any channel. If I need something from you I'll ping you like this <@${member.user.id}> :)`,
+			}
+		);
+	embed = addDefaultEmbedFooter(embed);
+
+	logger.debug("Someone new joined the guild");
+	if (member.guild.systemChannel) {
+		member.guild.systemChannel.send({ embeds: [embed] });
+	} else {
+		member.user.send({ embeds: [embed] });
+	}
+}
