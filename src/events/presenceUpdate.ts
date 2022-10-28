22cfb868e556910427bb5bae885dab8f482937bc
Floschy <69321390+flloschy@users.noreply.github.com>
2022-10-28 22:45:27 +0100

    thx
    Co-authored-by: Wilson <33164598+WilsontheWolf@users.noreply.github.com>

--- src/events/presenceUpdate.ts
+++ src/events/presenceUpdate.ts
@@ -13,8 +13,7 @@ export default async function presenceUpdate(oldPresence: Presence | null, newPr
 	stopedActivities.forEach(async (element) => {
 		const start = element.createdTimestamp;
 		const timePlayed = Date.now() - start;
-		// if (timePlayed < 20000) return;
-		// TODO: UNCOMMENT!!! IF I FORGET, PLEASE REMIND ME!!!!!!!!!
+		if (timePlayed < 20000) return;
 
 		if (await blacklistCheck(userid, element.name.toLowerCase())) return;
 		if (oldPresence?.user?.bot) return;
