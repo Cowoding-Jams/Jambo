ece5b21b12d7f5aea64770b0fa36773fd27b92d8
flloschy <floschy.email@gmail.com>
2022-11-11 17:46:19 +0100

    better Time documentation

--- src/util/birthdaySystem/TimeDefinition.ts
+++ src/util/birthdaySystem/TimeDefinition.ts
@@ -1,16 +1,16 @@
 // Time specified as Text (summer time)
 
-/** 7 o'clock  */
-export const Moring:number = 7;
-/** 11 o'clock */
-export const LateMoring:number = 11;
-/** 13 o'clock */
+/** 7 am  */
+export const Morning:number = 7;
+/** 11 am */
+export const LateMorning:number = 11;
+/** 1 pm */
 export const Midday:number = 13;
-/** 15 o'clock */
+/** 3  pm */
 export const Afternoon:number = 15;
-/** 18 o'clock */
+/** 6 pm */
 export const Evening:number = 18;
-/** 22 o'clock */
+/** 10 pm */
 export const Night:number = 22;
-/** 0 o'clock */
+/** 0 am */
 export const Midnight:number = 0;
