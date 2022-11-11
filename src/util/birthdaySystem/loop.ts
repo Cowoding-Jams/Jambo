e72805f5610043c0e886f66d5f31046f29673324
flloschy <floschy.email@gmail.com>
2022-11-11 20:32:15 +0100

    Birthday Message

--- src/util/birthdaySystem/loop.ts
+++ src/util/birthdaySystem/loop.ts
@@ -1,48 +1,50 @@
-import { Client, Guild } from "discord.js";
+import { Client, EmbedBuilder, Guild } from "discord.js";
 import { schedule } from "node-cron";
+import { config } from "../../config"
 import { birthdayDb } from "../../db";
-import { config } from "../../config";
 import "dotenv/config";
+import { addDefaultEmbedFooter } from "../misc/embeds";
 
 export async function BirthdayMessage(client: Client) {
-    if (!process.env.DEFAULT_GUILD) throw new Error("This error shouldn't accure... Something is wrong with the env.");
-    let guild = await client.guilds.fetch(process.env.DEFAULT_GUILD)
 
+  schedule("0 * * * *", async () => {
+    console.log("Execution")
+    if (!process.env.DEFAULT_GUILD) throw new Error("This error shouldn't accure... Something is wrong with the env");
 
-    // fuck this
-    for (let timezone = -12; timezone < 12; timezone++) {
-        schedule(`0 0 ${12+timezone} * * *`, async () => {
-            const birthdayUsers = birthdayDb.keyArray()
-            const timezoneUsers = getUserWithTimeZone(`UTC${timezone == 0 ? "" : timezone}`, guild); 
-            birthdayUsers.forEach((userid) => {
-                if (timezoneUsers?.includes(userid)) return
-                const entry = birthdayDb.get(userid)
-                if (!entry) return
-                const month = entry.month
-                const day = entry.day
+    const targetHour = config.birthdayNotificationAt - 1;
+    const date = new Date();
+    const timezone = targetHour - date.getUTCHours();
+    const defaultGuild = await client.guilds.fetch(process.env.DEFAULT_GUILD);
+    const possibleUsers = date.getUTCHours() == 0 ?
+      getMembersWithNoTimeZone(defaultGuild) || [] :
+      getMembersWithTimeZone(`UTC${(timezone > 0 ? "+" + timezone : (timezone < 0 ? timezone : ""))}`, defaultGuild) || []
+    const adjustedDate = new Date(Date.now() + timezone * 60 * 60 * 1000);
+    const adjustedMonth = adjustedDate.getUTCMonth() + 1;
+    const adjustedDay = adjustedDate.getUTCDate();
+    for (const user of possibleUsers) {
+      const { day, month } = birthdayDb.get(user.id) ?? {};
+      if (!day || !month) continue;
 
-                if ()
-
-            })
-        })
-
-
-
+      if (
+        (adjustedMonth === month && adjustedDay === day)
+        || (month === 1 && day === 29 && adjustedMonth === 2 && adjustedDay === 1)
+      ) {
+        const embed = addDefaultEmbedFooter(
+          new EmbedBuilder()
+            .setTitle("🎉🎉🎉 Birthday time 🎉🎉🎉")
+            .setThumbnail(user.displayAvatarURL({ size: 1024 }))
+            .setDescription("**CONGRATULATIONS!!!!!** IT'S YOUR **BIRTHDAY**")
+        );
+        await defaultGuild.systemChannel?.send({ content: user.toString(), embeds: [embed] }).catch((e) => null);
+      }
     }
-
-
-    schedule("0 0 12 * * * ", async () => {
-        birthdayDb.keyArray().forEach((userid) => {
-            const entry = birthdayDb.get(userid)
-            if (!entry) return
-            const month = entry.month
-            const day = entry.day
-
-
-        })
-    })
+  });
 }
 
-function getUserWithTimeZone(timezone:string, guild:Guild) {
-    return guild.roles.cache.find(role => role.name == timezone)?.members.map(m => m.user.id)
+function getMembersWithTimeZone(timezone: string, guild: Guild) {
+  return guild.roles.cache.find(role => role.name == timezone)?.members.map(m => m)
+}
+
+function getMembersWithNoTimeZone(guild: Guild) {
+  return guild.roles.cache.find(role => !role.name.startsWith("UTC"))?.members.map(m => m)
 }
