1768e6d8c5194a8c9024463354435dc6de787b2b
Murphy <me@murphy-in.tech>
2022-11-12 16:15:55 +0100

    upcoming command improvements

--- src/util/birthdayCommand/upcomingCommand.ts
+++ src/util/birthdayCommand/upcomingCommand.ts
@@ -8,58 +8,41 @@ export async function upcomingCommand(interaction: ChatInputCommandInteraction) 
 	const entrys = birthdayDb.keyArray();
 
 	let currentDate = new Date();
-	currentDate = new Date(
-		currentDate.getUTCFullYear(),
-		currentDate.getUTCMonth(),
-		currentDate.getUTCDate(),
-		0,
-		0,
-		0,
-		0
-	);
+	currentDate = new Date(currentDate.getUTCFullYear(), currentDate.getUTCMonth(), currentDate.getUTCDate());
 
-	let results: string[] = [];
+	let results: { userid: string; date: Date }[] = [];
 
 	entrys.forEach((userid) => {
 		const entry = birthdayDb.get(userid);
 		if (!entry) return;
-		const entryDate = new Date(currentDate.getUTCFullYear(), entry.month - 1, entry.day, 0, 0, 0, 0);
+		const entryDate = new Date(currentDate.getUTCFullYear(), entry.month - 1, entry.day);
+		const dayDifference = (entryDate.getTime() - currentDate.getTime()) / 86400000;
 
-		if (entry.month < currentDate.getUTCMonth()) return;
-		if (entry.day < currentDate.getUTCDate()) return;
-
-		const dayDifference = (currentDate.getTime() - entryDate.getTime()) / 86400000;
-
-		if (dayDifference <= 30 && dayDifference < 1) results.push(userid);
+		if (dayDifference <= 30 && dayDifference > 1) results.push({ userid, date: entryDate });
 	});
 
 	results = results
 		.sort((a, b) => {
-			const ea = birthdayDb.get(a);
-			const eb = birthdayDb.get(b);
-			return (ea?.day || 1) + (ea?.month || 1) * 31 - ((eb?.day || 1) + (ea?.month || 1) * 31);
+			return a.date.getTime() - b.date.getTime();
 		})
 		.slice(0, 10);
 
 	const list: string[] = [];
 
-	results.forEach((userid) => {
-		const entry = birthdayDb.get(userid);
-		if (!entry) return;
-		const entryDate = new Date(currentDate.getUTCFullYear(), entry.month - 1, entry.day, 0, 0, 0, 0);
-		list.push(`<@!${userid}> ⁘ <t:${entryDate.getTime() / 1000}:D>`);
+	results.forEach((entry) => {
+		list.push(`<@!${entry.userid}> ⁘ <t:${entry.date.getTime() / 1000}:D>`);
 	});
 
 	if (list.length == 0) {
 		let embed = new EmbedBuilder()
-			.setTitle("No upcoming Birthdays")
+			.setTitle("No upcoming birthdays")
 			.setDescription("Seems like there are no upcoming birthdays in the next 30 days");
 		embed = addDefaultEmbedFooter(embed);
 		await interaction.editReply({ embeds: [embed] });
 		return;
 	}
 
-	let embed = new EmbedBuilder().setTitle("Upcoming Birthdays").setDescription(list.join("\n"));
+	let embed = new EmbedBuilder().setTitle("Upcoming birthdays").setDescription(list.join("\n"));
 	embed = addDefaultEmbedFooter(embed);
 	await interaction.editReply({ embeds: [embed] });
 }
