28897bb7800a9605f544dcfd722fc7b0237c1a02
Murphy <me@murphy-in.tech>
2022-11-12 15:42:29 +0100

    feat: embed (#46)
    * init (gotta go to uni...)
    
    * done
    
    * prettify
    
    * solve issues
    
    Co-authored-by: Antti <antti@antti.codes>

--- src/util/misc/embeds.ts
+++ src/util/misc/embeds.ts
@@ -10,3 +10,18 @@ export function addDefaultEmbedFooter(embed: EmbedBuilder): EmbedBuilder {
 		})
 		.setColor(config.color);
 }
+
+export function countEmbedCharacters(embed: EmbedBuilder): number {
+	return [
+		embed.data.title,
+		embed.data.description,
+		embed.data.fields?.map((f) => [f.name, f.value]) ?? [],
+		embed.data.footer?.text,
+		embed.data.author?.name,
+	]
+		.flat(2)
+		.reduce((p, c) => {
+			if (typeof c === "string") return p + c.length;
+			return p;
+		}, 0);
+}
