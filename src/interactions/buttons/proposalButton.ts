273c78c238d6651115bc607f9b7c0234958258f0
Murphy <git@murphy-in.space>
2022-11-13 11:30:39 +0100

    page list improvements

--- src/interactions/buttons/proposalButton.ts
+++ src/interactions/buttons/proposalButton.ts
@@ -10,10 +10,15 @@ class ProposalButton extends Button {
 	async execute(interaction: ButtonInteraction, args: string[]): Promise<void> {
 		if (args[0] == "list") {
 			const page = parseInt(args[2]);
+			const proposalsPerPage = parseInt(args[3]);
 			if (args[1] == "left") {
-				listProposals(interaction, page - 1);
+				listProposals(interaction, page - 1, proposalsPerPage);
 			} else if (args[1] == "right") {
-				listProposals(interaction, page + 1);
+				listProposals(interaction, page + 1, proposalsPerPage);
+			} else if (args[1] == "minus") {
+				listProposals(interaction, page, proposalsPerPage > 1 ? proposalsPerPage - 1 : 1);
+			} else if (args[1] == "plus") {
+				listProposals(interaction, page, proposalsPerPage < 10 ? proposalsPerPage + 1 : 10);
 			}
 		}
 	}
--- src/util/proposalCommand/listProposals.ts
+++ src/util/proposalCommand/listProposals.ts
@@ -1,19 +1,19 @@
 import {
 	ActionRowBuilder,
 	ButtonBuilder,
+	ButtonInteraction,
 	ButtonStyle,
 	ChatInputCommandInteraction,
 	EmbedBuilder,
-	ButtonInteraction,
 } from "discord.js";
 import { proposalDb } from "../../db";
 import { addEmbedColor } from "../misc/embeds";
 
 export async function listProposals(
 	interaction: ChatInputCommandInteraction | ButtonInteraction,
-	page: number = 0
+	page = 0,
+	proposalsPerPage = 2 // up to 10 embeds per message
 ): Promise<void> {
-	const proposalsPerPage = 5;
 	const pages = Math.ceil(proposalDb.size / proposalsPerPage);
 
 	if (page < 0) page = pages - 1;
@@ -24,27 +24,51 @@ export async function listProposals(
 		.map((p) => p)
 		.slice(page * proposalsPerPage, (page + 1) * proposalsPerPage);
 
-	let embed = addEmbedColor(
-		new EmbedBuilder().setTitle("Proposals").setFooter({ text: `Page ${page + 1}/${pages}` })
-	);
+	let embeds: EmbedBuilder[] = [];
 
 	for (const p of proposals) {
-		embed.addFields({
-			name: p.title,
-			value: `${p.description}${p.references != "" ? `\nReferences:\n${p.references}` : ""}\nProposed By: ${
-				(await interaction.guild?.members.fetch(p.owner))?.toString() || "Unknown"
-			} - Proposed Time Period: ${p.timePeriod}`,
-		});
+		embeds.push(
+			addEmbedColor(new EmbedBuilder().setTitle(p.title).setDescription(p.description)).addFields(
+				{
+					name: "References",
+					value: p.references ? p.references : "-",
+				},
+				{ name: "Proposed Time Period", value: p.timePeriod, inline: true },
+				{
+					name: "Proposed By",
+					value: (await interaction.guild?.members.fetch(p.owner))?.toString() || "Unknown",
+					inline: true,
+				}
+			)
+		);
 	}
 
 	const row = new ActionRowBuilder<ButtonBuilder>().setComponents(
-		new ButtonBuilder().setCustomId(`proposal.list.left.${page}`).setLabel("◀").setStyle(ButtonStyle.Primary),
-		new ButtonBuilder().setCustomId(`proposal.list.right.${page}`).setLabel("▶").setStyle(ButtonStyle.Primary)
+		new ButtonBuilder()
+			.setCustomId(`proposal.list.minus.${page}.${proposalsPerPage}`)
+			.setLabel("-1")
+			.setStyle(ButtonStyle.Secondary),
+		new ButtonBuilder()
+			.setCustomId(`proposal.list.left.${page}.${proposalsPerPage}`)
+			.setLabel("◀")
+			.setStyle(ButtonStyle.Primary),
+		new ButtonBuilder()
+			.setCustomId(`proposal.list.right.${page}.${proposalsPerPage}`)
+			.setLabel("▶")
+			.setStyle(ButtonStyle.Primary),
+		new ButtonBuilder()
+			.setCustomId(`proposal.list.plus.${page}.${proposalsPerPage}`)
+			.setLabel("+1")
+			.setStyle(ButtonStyle.Secondary)
 	);
 
-	if (proposalDb.size == 0) embed = addEmbedColor(new EmbedBuilder().setTitle("No proposals in yet..."));
+	if (proposalDb.size == 0) embeds = [addEmbedColor(new EmbedBuilder().setTitle("No proposals in yet..."))];
+	else
+		embeds[embeds.length - 1].setFooter({
+			text: `Page ${page + 1}/${pages} - Proposals per page: ${proposalsPerPage}/10`,
+		});
 
 	if (interaction instanceof ChatInputCommandInteraction)
-		await interaction.reply({ embeds: [embed], components: proposalDb.size != 0 ? [row] : [] });
-	else await interaction.update({ embeds: [embed], components: proposalDb.size != 0 ? [row] : [] });
+		await interaction.reply({ embeds: embeds, components: proposalDb.size != 0 ? [row] : [] });
+	else await interaction.update({ embeds: embeds, components: proposalDb.size != 0 ? [row] : [] });
 }
