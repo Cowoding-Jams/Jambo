495f268dc7f47320fa35727e1e568c831d679e51
Murphy <git@murphy-in.space>
2023-01-15 11:21:07 +0100

    catch statements

--- src/util/misc/verify.ts
+++ src/util/misc/verify.ts
@@ -10,11 +10,17 @@ export async function emojiExists(guild: Guild, emoji: ComponentEmojiResolvable)
 }
 
 export async function roleExists(guild: Guild, role: string): Promise<boolean> {
-	return await guild.roles.fetch(role).then((role) => role !== null);
+	return await guild.roles
+		.fetch(role)
+		.catch(() => false)
+		.then((role) => role !== null);
 }
 
 export async function channelExists(guild: Guild, channel: string): Promise<boolean> {
-	return await guild.channels.fetch(channel).then((channel) => channel !== null);
+	return await guild.channels
+		.fetch(channel)
+		.catch(() => false)
+		.then((channel) => channel !== null);
 }
 
 export function isValidHexColor(color: string): boolean {
