02af2e5939e79166718e640a1aee7f45762e9466
Murphy <git@murphy-in.space>
2023-01-25 16:25:39 +0100

    fix: proposal add (#57)
    * fix
    
    * fix not building
    
    * fix

--- src/util/proposal/manageProposals.ts
+++ src/util/proposal/manageProposals.ts
@@ -54,11 +54,11 @@ export async function addProposal(interaction: ChatInputCommandInteraction): Pro
 	const modal = new ModalBuilder().setCustomId("proposal.add").setTitle("Add a new proposal!");
 
 	modal.addComponents(
-		new ActionRowBuilder<TextInputBuilder>().addComponents(title.setValue("")),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(abbreviation.setValue("")),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(description.setValue("")),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(duration.setValue("")),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(references.setValue(""))
+		new ActionRowBuilder<TextInputBuilder>().addComponents(title),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(abbreviation),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(description),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(duration),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(references)
 	);
 
 	await interaction.showModal(modal);
@@ -121,11 +121,19 @@ export async function editProposal(interaction: ChatInputCommandInteraction): Pr
 	const modal = new ModalBuilder().setCustomId(`proposal.edit.${key}`).setTitle("Edit a proposal!");
 
 	modal.addComponents(
-		new ActionRowBuilder<TextInputBuilder>().addComponents(title.setValue(proposal.title)),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(abbreviation.setValue(proposal.abbreviation)),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(description.setValue(proposal.description)),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(duration.setValue(proposal.duration.toISO())),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(references.setValue(proposal.references))
+		new ActionRowBuilder<TextInputBuilder>().addComponents(structuredClone(title).setValue(proposal.title)),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(
+			structuredClone(abbreviation).setValue(proposal.abbreviation)
+		),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(
+			structuredClone(description).setValue(proposal.description)
+		),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(
+			structuredClone(duration).setValue(proposal.duration.toISO())
+		),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(
+			structuredClone(references).setValue(proposal.references)
+		)
 	);
 
 	await interaction.showModal(modal);
