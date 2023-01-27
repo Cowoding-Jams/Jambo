5374588639fd4433ecba95c2cf377f9fa2b709a5
Murphy <git@murphy-in.space>
2023-01-27 21:48:00 +0100

    fix: proposal and debug mode (#60)

--- src/util/proposal/manageProposals.ts
+++ src/util/proposal/manageProposals.ts
@@ -8,57 +8,62 @@ import {
 import { pollDb, proposalDb } from "../../db";
 import { hasAdminRole } from "../misc/permissions";
 
-const title = new TextInputBuilder()
-	.setCustomId("title")
-	.setLabel("Title (Must be unique)")
-	.setPlaceholder("Enter a catchy title for your proposal!")
-	.setStyle(TextInputStyle.Short)
-	.setMinLength(2)
-	.setMaxLength(60);
+const title = () =>
+	new TextInputBuilder()
+		.setCustomId("title")
+		.setLabel("Title (Must be unique)")
+		.setPlaceholder("Enter a catchy title for your proposal!")
+		.setStyle(TextInputStyle.Short)
+		.setMinLength(2)
+		.setMaxLength(60);
 
-const abbreviation = new TextInputBuilder()
-	.setCustomId("abbreviation")
-	.setLabel("Shorter title/abbreviation (in kebab-case)")
-	.setPlaceholder("(e.g. 'aoc22' instead of 'Advent of Code 2022')")
-	.setStyle(TextInputStyle.Short)
-	.setMinLength(2)
-	.setMaxLength(20);
+const abbreviation = () =>
+	new TextInputBuilder()
+		.setCustomId("abbreviation")
+		.setLabel("Shorter title/abbreviation (in kebab-case)")
+		.setPlaceholder("(e.g. 'aoc22' instead of 'Advent of Code 2022')")
+		.setStyle(TextInputStyle.Short)
+		.setMinLength(2)
+		.setMaxLength(20);
 
-const description = new TextInputBuilder()
-	.setCustomId("description")
-	.setLabel("Description")
-	.setPlaceholder("Add a suiting description!")
-	.setStyle(TextInputStyle.Paragraph)
-	.setMinLength(20)
-	.setMaxLength(200);
+const description = () =>
+	new TextInputBuilder()
+		.setCustomId("description")
+		.setLabel("Description")
+		.setPlaceholder("Add a suiting description!")
+		.setStyle(TextInputStyle.Paragraph)
+		.setMinLength(20)
+		.setMaxLength(200);
 
-const duration = new TextInputBuilder()
-	.setCustomId("duration")
-	.setLabel("Duration in the ISO Duration format")
-	.setPlaceholder("(e.g. 'P2DT12H' which corresponds to 2 days and 12 hours)")
-	.setStyle(TextInputStyle.Short)
-	.setMinLength(2)
-	.setMaxLength(20);
+const duration = () =>
+	new TextInputBuilder()
+		.setCustomId("duration")
+		.setLabel("Duration in the ISO Duration format")
+		.setPlaceholder("(e.g. 'P2DT12H' which corresponds to 2 days and 12 hours)")
+		.setStyle(TextInputStyle.Short)
+		.setMinLength(2)
+		.setMaxLength(20);
 
-const references = new TextInputBuilder()
-	.setCustomId("references")
-	.setLabel("References")
-	.setPlaceholder(
-		"Add any references you may have like links to videos! (please make it a pretty bullet list)"
-	)
-	.setStyle(TextInputStyle.Paragraph)
-	.setMaxLength(200)
-	.setRequired(false);
+const references = () =>
+	new TextInputBuilder()
+		.setCustomId("references")
+		.setLabel("References")
+		.setPlaceholder(
+			"Add any references you may have like links to videos! (please make it a pretty bullet list)"
+		)
+		.setStyle(TextInputStyle.Paragraph)
+		.setMaxLength(200)
+		.setRequired(false);
 
 export async function addProposal(interaction: ChatInputCommandInteraction): Promise<void> {
 	const modal = new ModalBuilder().setCustomId("proposal.add").setTitle("Add a new proposal!");
 
 	modal.addComponents(
-		new ActionRowBuilder<TextInputBuilder>().addComponents(title),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(abbreviation),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(description),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(duration),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(references)
+		new ActionRowBuilder<TextInputBuilder>().addComponents(title()),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(abbreviation()),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(description()),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(duration()),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(references())
 	);
 
 	await interaction.showModal(modal);
@@ -121,19 +126,11 @@ export async function editProposal(interaction: ChatInputCommandInteraction): Pr
 	const modal = new ModalBuilder().setCustomId(`proposal.edit.${key}`).setTitle("Edit a proposal!");
 
 	modal.addComponents(
-		new ActionRowBuilder<TextInputBuilder>().addComponents(structuredClone(title).setValue(proposal.title)),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(
-			structuredClone(abbreviation).setValue(proposal.abbreviation)
-		),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(
-			structuredClone(description).setValue(proposal.description)
-		),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(
-			structuredClone(duration).setValue(proposal.duration.toISO())
-		),
-		new ActionRowBuilder<TextInputBuilder>().addComponents(
-			structuredClone(references).setValue(proposal.references)
-		)
+		new ActionRowBuilder<TextInputBuilder>().addComponents(title().setValue(proposal.title)),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(abbreviation().setValue(proposal.abbreviation)),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(description().setValue(proposal.description)),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(duration().setValue(proposal.duration.toISO())),
+		new ActionRowBuilder<TextInputBuilder>().addComponents(references().setValue(proposal.references))
 	);
 
 	await interaction.showModal(modal);
