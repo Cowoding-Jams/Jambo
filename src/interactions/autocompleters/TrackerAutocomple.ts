973db52f65d3e3b5c0ee460023e488c853927e70
flloschy <floschy.email@gmail.com>
2023-04-23 14:04:01 +0200

    adaptive autocomplete (a bit to many nested if's, but works)

--- src/interactions/autocompleters/TrackerAutocomple.ts
+++ src/interactions/autocompleters/TrackerAutocomple.ts
@@ -1,16 +1,63 @@
 import { Autocompleter } from "../interactionClasses";
 import { AutocompleteInteraction } from "discord.js";
-import { trackerGames } from "../../db";
-
+import { trackerBlacklist, trackerGames, trackerUsers } from "../../db";
 class Tracker extends Autocompleter {
 	constructor() {
 		super("tracker");
 	}
 
 	async execute(interaction: AutocompleteInteraction): Promise<void> {
-		const games = Array.from(trackerGames.keys());
+		const sub = interaction.options.getSubcommand();
 		const focus = interaction.options.getFocused().toLowerCase() as string;
-		let filteredOptions = games.filter((g) => g.toLowerCase().startsWith(focus));
+		const option = interaction.options.getFocused(true).name;
+		const user =
+			typeof interaction.options.get("user")?.value == "string"
+				? interaction.options.get("user")?.value
+				: interaction.user.id;
+		const game = interaction.options.get("game")?.value;
+		const action =
+			typeof interaction.options.get("action")?.value == "string"
+				? interaction.options.get("action")?.value
+				: "add";
+		const options: string[] = [];
+
+		if (sub == "user") {
+			if (option == "game") {
+				if (typeof user == "string") {
+					if (trackerUsers.get(user)?.games) {
+						trackerUsers.get(user)?.games.forEach((g) => options.push(g.id));
+					} else {
+						options.push("USER HAS NOT PLAYED ANY GAMES YET");
+					}
+				} else {
+					options.push(...trackerGames.keyArray());
+				}
+			} else if (option == "statistic") {
+				if (typeof game == "string") {
+					options.push(...["playtime", "logs"]);
+				} else {
+					options.push(
+						...["general statistics", "top 5 most played games", "top 5 most logged games", "latest 5 logs"]
+					);
+				}
+			}
+		} else if (sub == "game") {
+			trackerGames.keyArray().forEach((e) => options.push(e));
+		} else if (sub == "blacklist") {
+			if (action == "add") {
+				options.push(
+					...trackerGames.keyArray().filter((e) => !trackerBlacklist.get("")?.find((e2) => e2 == e))
+				);
+			} else if (action == "rem") {
+				options.push(
+					...trackerGames.keyArray().filter((e) => !!trackerBlacklist.get("")?.find((e2) => e2 == e))
+				);
+			} else {
+				options.push(...trackerGames.keyArray());
+			}
+		}
+
+		let filteredOptions = options.filter((o) => o.toLowerCase().startsWith(focus));
 		if (filteredOptions.length > 25) filteredOptions = filteredOptions.slice(0, 25);
 
 		const map = filteredOptions.map((c) => ({ name: c, value: c }));
