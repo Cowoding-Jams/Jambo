cb2d74aa8745f0f867d00334cb790f4521c72014
flloschy <floschy.email@gmail.com>
2023-04-26 20:58:38 +0200

    export nested ifs and comments

--- src/interactions/autocompleters/TrackerAutocomple.ts
+++ src/interactions/autocompleters/TrackerAutocomple.ts
@@ -1,6 +1,8 @@
 import { Autocompleter } from "../interactionClasses";
 import { AutocompleteInteraction } from "discord.js";
 import { trackerBlacklist, trackerGames, trackerUsers } from "../../db";
+import { blacklist, user } from "../../util/tracker/autocompleteHelper";
+
 class Tracker extends Autocompleter {
 	constructor() {
 		super("tracker");
@@ -10,59 +12,37 @@ class Tracker extends Autocompleter {
 		const sub = interaction.options.getSubcommand();
 		const focus = interaction.options.getFocused().toLowerCase() as string;
 		const option = interaction.options.getFocused(true).name;
-		const user =
+		
+		// if user-option is given, use it, else default to executer
+		const userId =
 			typeof interaction.options.get("user")?.value == "string"
 				? interaction.options.get("user")?.value
 				: interaction.user.id;
 		const game = interaction.options.get("game")?.value;
+
+		// if action-option is given, use it, else default to add
 		const action =
 			typeof interaction.options.get("action")?.value == "string"
 				? interaction.options.get("action")?.value
 				: "add";
-		const options: string[] = [];
+
+		// make ts happy...
+		if (typeof action != "string") return 
+
+		let options: string[] = [];
 
 		if (sub == "user") {
-			if (option == "game") {
-				if (typeof user == "string") {
-					if (trackerUsers.get(user)?.games) {
-						trackerUsers.get(user)?.games.forEach((g) => options.push(g.id));
-					} else {
-						options.push("[user has not played any games yet]");
-					}
-				} else {
-					options.push(...trackerGames.keyArray());
-				}
-			} else if (option == "statistic") {
-				if (typeof game == "string") {
-					options.push(...["playtime", "logs"]);
-				} else {
-					options.push(
-						...["general statistics", "top 5 most played games", "top 5 most logged games", "latest 5 logs"]
-					);
-				}
-			}
+			options = user(option, userId, game);
 		} else if (sub == "game") {
 			trackerGames.keyArray().forEach((e) => options.push(e));
 		} else if (sub == "blacklist") {
-			if (action == "add") {
-				options.push(
-					...trackerGames.keyArray().filter((e) => !trackerBlacklist.get("")?.find((e2) => e2 == e))
-				);
-			} else if (action == "rem") {
-				options.push(
-					...trackerGames.keyArray().filter((e) => !!trackerBlacklist.get("")?.find((e2) => e2 == e))
-				);
-			} else {
-				options.push(...trackerGames.keyArray());
-			}
+			options = blacklist(action)
 		}
 
 		let filteredOptions = options.filter((o) => o.toLowerCase().startsWith(focus));
-		if (filteredOptions.length > 25) filteredOptions = filteredOptions.slice(0, 25);
+		filteredOptions = filteredOptions.slice(0, 25);
 
-		const map = filteredOptions.map((c) => ({ name: c, value: c }));
-
-		await interaction.respond(map);
+		await interaction.respond(filteredOptions.map((c) => ({ name: c, value: c })));
 	}
 }
 
---
+++ src/util/tracker/autocompleteHelper.ts
@@ -0,0 +1,45 @@
+import { trackerBlacklist, trackerGames, trackerUsers } from "../../db";
+
+
+export function user(option:string, userid:string | number | boolean | undefined, game:string | number | boolean | undefined) {
+
+    let results = []
+
+    if (option == "game") {
+        if (typeof userid == "string") {
+            if (trackerUsers.get(userid)?.games) {
+                // list games a user has played (if user has played anything)
+                trackerUsers.get(userid)?.games.forEach((g) => results.push(g.id));
+            } else {
+                // show this when user didnt play any games
+                results.push("[user has not played any games yet]");
+            }
+        } else {
+            // list all games in case user hasnt filled in the user-option yet
+            results.push(...trackerGames.keyArray());
+        }
+    } else if (option == "statistic") {
+            // only show playtime and logs if a game is given, else show standard options
+            results.push(...(typeof game == "string" ? ["playtime", "logs"] : ["general statistics", "top 5 most played games", "top 5 most logged games", "latest 5 logs"]));
+    }
+
+    return results
+
+}
+
+export function blacklist(action:string) {
+
+    let results = []
+
+    if (action == "add") {
+        // only show not blacklisted games when "add" is chosen as the action, else only show blacklisted games
+        results.push(
+            ...trackerGames.keyArray().filter((e) => (action=="add" ? !trackerBlacklist.get("")?.find((e2) => e2 == e) : !!trackerBlacklist.get("")?.find((e2) => e2 == e)))
+        )
+    } else {
+        // show all games in case action-option isnt filled in yet
+        results.push(...trackerGames.keyArray());
+    }
+
+    return results
+}
