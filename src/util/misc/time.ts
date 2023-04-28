fa10720f3aa94a9cc2e8cce27e7cef23e16196c7
flloschy <floschy.email@gmail.com>
2023-04-28 16:54:26 +0200

    formatting

--- src/util/misc/time.ts
+++ src/util/misc/time.ts
@@ -2,64 +2,65 @@ import { ChatInputCommandInteraction, CommandInteraction, ModalSubmitInteraction
 import { DateTime, Duration } from "luxon";
 
 function makeTimestamp(datetime: DateTime | number, end?: string): string {
-	return `<t:${datetime instanceof DateTime ? Math.floor(datetime.toSeconds()) : datetime}${end ? ":" + end : ""}>`;
+	return `<t:${datetime instanceof DateTime ? Math.floor(datetime.toSeconds()) : datetime}${
+		end ? ":" + end : ""
+	}>`;
 }
 
-/** 
+/**
  * Make a discord Timestamp which displays like this: `1 January 1970 01:00` \
  * @param datetime DateTime object or number in Seconds */
 export function discordTimestamp(datetime: DateTime | number): string {
 	return makeTimestamp(datetime);
 }
-/** 
+/**
  * Make a discord Timestamp which displays like this: `53 years ago` \
  * @param datetime DateTime object or number in Seconds */
 export function discordRelativeTimestamp(datetime: DateTime | number): string {
 	return makeTimestamp(datetime, "R");
 }
-/** 
+/**
  * Make a discord Timestamp which displays like this: `01:00` \
  * @param datetime DateTime object or number in Seconds */
 export function discordShortTimeTimestamp(datetime: DateTime | number): string {
 	return makeTimestamp(datetime, "t");
 }
-/** 
+/**
  * Make a discord Timestamp which displays like this: `01:00:00` \
  * @param datetime DateTime object or number in Seconds */
 export function discordLongTimeTimestamp(datetime: DateTime | number): string {
 	return makeTimestamp(datetime, "T");
 }
-/** 
+/**
  * Make a discord Timestamp which displays like this: `01/01/1970` \
  * @param datetime DateTime object or number as Seconds */
 export function discordShortDateTimestamp(datetime: DateTime | number): string {
 	return makeTimestamp(datetime, "d");
 }
-/** 
+/**
  * Make a discord Timestamp which displays like this: `1 January 1970` \
  * @param datetime DateTime object or number in Seconds */
 export function discordLongDateTimestamp(datetime: DateTime | number): string {
 	return makeTimestamp(datetime, "D");
 }
-/** 
+/**
  * Make a discord Timestamp which displays like this: `1 January 1970 01:00` \
  * @param datetime DateTime object or number in Seconds */
 export function discordLongDateWithShortTimeTimestamp(datetime: DateTime | number): string {
 	return makeTimestamp(datetime, "f");
 }
-/** 
+/**
  * Make a discord Timestamp which displays like this: `Thursday, 1 January 1970 01:00` \
  * @param datetime DateTime object or number in Seconds */
 export function discordLongDateWithDateOfWeekAndShortTimeTimestamp(datetime: DateTime | number): string {
 	return makeTimestamp(datetime, "F");
 }
-/** 
+/**
  * Make a custom Timestamp which displays like this: `01/01/1970 01:00` \
  * @param datetime DateTime object or number in Seconds */
 export function shortDateAndShortTimeTimestamp(datetime: DateTime | number): string {
-	return discordShortDateTimestamp(datetime) + discordShortTimeTimestamp(datetime)
+	return discordShortDateTimestamp(datetime) + discordShortTimeTimestamp(datetime);
 }
-
 
 export function durationToReadable(duration: Duration, short = false): string {
 	if (short) {
@@ -116,5 +117,5 @@ export const dayInMillis = dayInSeconds * 1000;
 export const weekInSeconds = dayInSeconds * 7;
 export const weekInMillis = dayInMillis * 7;
 // 4 times a week + 2.4 days = (average) month
-export const monthInSeconds = weekInSeconds * 4 + dayInSeconds * 2.4167
+export const monthInSeconds = weekInSeconds * 4 + dayInSeconds * 2.4167;
 export const monthInMillis = weekInMillis * 4 + dayInMillis * 2.4167;
